9e64762fe42bc5465d2ebab7d98ce074ff0720e6
lulzsoft <lulzsoft@users.noreply.github.com>
2026-01-23 02:05:08 +0300

    Fix: Change redis import from default to named import - fixes Vercel build

--- src/lib/mission-registry.ts
+++ src/lib/mission-registry.ts
@@ -1,4 +1,4 @@
-import redis from './redis';
+import { redis } from './redis';
 import { Mission, MissionStatus, MissionApplication } from '@/types/mission';
 
 /**
--- src/lib/squad-registry.ts
+++ src/lib/squad-registry.ts
@@ -1,4 +1,4 @@
-import redis from './redis';
+import { redis } from './redis';
 import { Squad, SquadStatus } from '@/types/squad';
 
 /**
